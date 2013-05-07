f199ac6b08720a1b9a87186be2c03a10a5c2e803
tzwm <wangjunzhe4444@gmail.com>
2013-05-07 17:41:15 +0800

    finish twitter oauth and save access_token

--- handlers.py
+++ handlers.py
@@ -4,18 +4,32 @@ from google.appengine.api import users
 from google.appengine.ext import db
 
 import urlparse
+from webapp2_extras import sessions
+import webapp2
 
 import lib.oauth2 as oauth
 import db_model
 import config
 
-# twitgreeting = "test"
-# twitvisit = "test"
+class BaseHandler(webapp2.RequestHandler):
+    def dispatch(self):                                 # override dispatch
+        # Get a session store for this request.
+        self.session_store = sessions.get_store(request=self.request)
 
-class RootHandler(webapp.RequestHandler):
+        try:
+            # Dispatch the request.
+            webapp2.RequestHandler.dispatch(self)       # dispatch the main handler
+        finally:
+            # Save all sessions.
+            self.session_store.save_sessions(self.response)
+
+    @webapp2.cached_property
+    def session(self):
+        # Returns a session using the default cookie key.
+        return self.session_store.get_session()
+
+class RootHandler(BaseHandler):
     def get(self):
-        # global twitgreeting, twitvisit
-
         user = users.get_current_user()
         if user:
             greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
@@ -25,33 +39,56 @@ class RootHandler(webapp.RequestHandler):
             if not clients.get():
                 client = db_model.Client(email=user.email())
                 client.put()
-                clients.fetch()
+                clients.get()
+                self.redirect('/')
+
 
             twitter_actoken_key = clients.get().twitter_actoken_key
-            twitter_actoken_secret = clients.get().twitter_actoken_secret
-
-            twitgreeting = ""    
-            twitvisit = ""
-
+            twitgreeting = ""
             if twitter_actoken_key:
-                twitgreeting = ("ok, %s." % twitter_actoken_key)
+                twitgreeting = ("twitter auth ok."))
             else:
-                consumer = oauth.Consumer(config.CONSUMER_KEY, config.CONSUMER_SECRET)
-                client = oauth.Client(consumer)
-
-                resp, content = client.request(config.REQUEST_TOKEN_URL, "GET")
-                if resp['status'] != '200':
-                    raise Exception("Invalid response %s." % resp['status'])
-                request_token = dict(urlparse.parse_qsl(content))
-
-                twitvisit = ("%s?oauth_token=%s" % (config.AUTHORIZE_URL, request_token['oauth_token']))
-                twitgreeting = ("to authenticate")
+                twitgreeting = ('<a href="/twitter/oauth/">twitter:sign in</a>')
         else:
             greeting = ("<a href=\"%s\">Sign in</a>" % 
                         users.create_login_url("/"))
             twitgreeting = ""
-            twitvisit = ""
 
-        self.response.out.write("<html><body>%s<br>%s<br>%s</body></html>" % (greeting, twitgreeting, twitvisit))
+        self.response.out.write("<html><body>%s<br>%s</body></html>" % (greeting, twitgreeting))
 
-                
+
+class TwitterHandler(BaseHandler):
+    def auth(self):
+        consumer = oauth.Consumer(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+        client = oauth.Client(consumer)
+
+        resp, content = client.request(config.REQUEST_TOKEN_URL, "GET")
+        if resp['status'] != '200':
+            raise Exception("Invalid response %s." % resp['status'])
+        request_token = dict(urlparse.parse_qsl(content))
+
+        self.session['req_token'] = request_token
+
+        self.redirect('%s?oauth_token=%s' % (config.AUTHORIZE_URL, request_token['oauth_token']))
+
+
+    def callback(self):
+        verifier = self.request.get('oauth_verifier')
+        request_token = self.session.pop('req_token', None)
+        token = oauth.Token(request_token['oauth_token'],
+                            request_token['oauth_token_secret'])
+        token.set_verifier(verifier)
+        consumer = oauth.Consumer(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+        client = oauth.Client(consumer, token)
+
+        resp, content = client.request(config.ACCESS_TOKEN_URL, "POST")
+        access_token = dict(urlparse.parse_qsl(content))
+
+        user = users.get_current_user()
+        clients = db.GqlQuery("SELECT * FROM Client", email=user.email())
+        client = clients.get()
+        client.twitter_actoken_key = access_token['oauth_token']
+        client.twitter_actoken_secret = access_token['oauth_token_secret']
+        client.put()
+
+        self.redirect('/')
--- main.py
+++ main.py
@@ -2,14 +2,28 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from webapp2 import WSGIApplication, Route
 
+import config
+
 routes = [
-  Route('/', handler='handlers.RootHandler')
+  Route('/', handler='handlers.RootHandler'),
+  Route('/twitter/oauth/', handler='handlers.TwitterHandler:auth'),
+  Route('/twitter/callback/', handler='handlers.TwitterHandler:callback')
 ]
 
-app = WSGIApplication(routes, debug=True)
+app_config = {
+  'webapp2_extras.sessions':{
+    'secret_key': config.SESSION_KEY
+  }
+}
+
+app = WSGIApplication(routes, config=app_config, debug=True)
+
+
 
 def main():
+
     run_wsgi_app(app)
+
 
 if __name__ == '__main__':
     main()
