7b120a65b15e49866a71c1fd108c2927083dad79
tzwm <wangjunzhe4444@gmail.com>
2013-05-07 09:53:43 +0800

    fix bug about 'referenced before assignment'

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-consumer_key = '5NqwNhDi3cLH7aMhzUbzRg'
-consumer_secret = 'I7AlLoODk5ckR8kKV0Tb3B9nPfbaBcnWY13OFf2fdI'
+CONSUMER_KEY = '5NqwNhDi3cLH7aMhzUbzRg'
+CONSUMER_SECRET = 'I7AlLoODk5ckR8kKV0Tb3B9nPfbaBcnWY13OFf2fdI'
 
 REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
 ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
--- handlers.py
+++ handlers.py
@@ -14,39 +14,44 @@ import config
 
 class RootHandler(webapp.RequestHandler):
     def get(self):
-    	# global twitgreeting, twitvisit
+        # global twitgreeting, twitvisit
 
-    	user = users.get_current_user()
-    	if user:
-    		greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
-    					(user.nickname(), users.create_logout_url("/")))
+        user = users.get_current_user()
+        if user:
+            greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
+                        (user.nickname(), users.create_logout_url("/")))
 
-    		clients = db.GqlQuery("SELECT * FROM Client", email=user.email())
-    		if not clients.get():
-    			client = db_model.Client(email=user.email())
-    			client.put()
-    			clients.fetch()
+            clients = db.GqlQuery("SELECT * FROM Client", email=user.email())
+            if not clients.get():
+                client = db_model.Client(email=user.email())
+                client.put()
+                clients.fetch()
 
-			twitter_actoken_key = clients.get().twitter_actoken_key
-			twitter_actoken_secret = clients.get().twitter_actoken_secret
-			
-			if twitter_actoken_key:
-				twitgreeting = ("ok, %s." % twitter_actoken_key)
-			else:
-				consumer - oauth.Consumer(config.CONSUMER_KEY, config.CONSUMER_SECRET)
-				client = oauth.Client(consumer)
+            twitter_actoken_key = clients.get().twitter_actoken_key
+            twitter_actoken_secret = clients.get().twitter_actoken_secret
 
-				resp, content = client.request(config.REQUEST_TOKEN_URL, "GET")
-				if resp['status'] != '200':
-					raise Exception("Invalid response %s." % resp['status'])
-				request_token = dict(urlparse.parse_qsl(content))
+            twitgreeting = ""    
+            twitvisit = ""
 
-				twitvisit = ("%s?oauth_token=%s" % (config.AUTHORIZE_URL, request_token['oauth_token']))
-				twitgreeting = ("to authenticate")
-    	else:
-    		greeting = ("<a href=\"%s\">Sign in</a>" % 
-    					users.create_login_url("/"))
+            if twitter_actoken_key:
+                twitgreeting = ("ok, %s." % twitter_actoken_key)
+            else:
+                consumer = oauth.Consumer(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+                client = oauth.Client(consumer)
 
-    	self.response.out.write("<html><body>%s<br>%s<br>%s</body></html>" % (greeting, twitgreeting, twitvisit))
+                resp, content = client.request(config.REQUEST_TOKEN_URL, "GET")
+                if resp['status'] != '200':
+                    raise Exception("Invalid response %s." % resp['status'])
+                request_token = dict(urlparse.parse_qsl(content))
 
-    			
+                twitvisit = ("%s?oauth_token=%s" % (config.AUTHORIZE_URL, request_token['oauth_token']))
+                twitgreeting = ("to authenticate")
+        else:
+            greeting = ("<a href=\"%s\">Sign in</a>" % 
+                        users.create_login_url("/"))
+            twitgreeting = ""
+            twitvisit = ""
+
+        self.response.out.write("<html><body>%s<br>%s<br>%s</body></html>" % (greeting, twitgreeting, twitvisit))
+
+                
